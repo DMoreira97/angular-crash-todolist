8fcd78ea3ee864aefd73060ef9f9edd908368d2a
Diogo Moreira <diogom.o.moreira@gmail.com>
2020-04-07 14:32:38 +0100

    Tutorial Done

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name:string = 'angularYEYEYEY';
+
+/*   constructor() {
+      this.changeName("john")
+  }
+
+  changeName(name:string):void {
+    this.name = name;
+  } */
+}
---
+++ src/app/models/Todo.ts
@@ -0,0 +1,7 @@
+export class Todo {
+    id:number;
+    /* id?:number; - question mark is for optional and not required */
+    title:string;
+    completed:boolean;
+
+}
